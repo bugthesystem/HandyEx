ee1902432335b9f88519967675c4b694bc6aa733
ziλa sarikaya <sarikayaziya@gmail.com>
2018-02-02 11:42:41 +0100

    Update TaskExtensions.cs

--- TaskExtensions.cs
+++ TaskExtensions.cs
@@ -1,20 +1,30 @@
-/// <summary>
+namespace HandyEx
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
     /// The task extensions.
     /// </summary>
     public static class TaskExtensions
     {
         /// <summary>
-        /// It processes an enumerable collection and creates a task that will complete 
-        /// when all of the <see cref="T:System.Threading.Tasks.Task`1" /> 
+        /// It processses an enumerable collection and creates a task that will complete when all of the <see cref="T:System.Threading.Tasks.Task`1" /> 
         /// objects in an enumerable collection have completed with limited concurrency.
         /// </summary>
         /// <param name="items">The items.</param>
-        /// <param name="taskFunc">The Task factory.</param>
+        /// <param name="func">The Task factory.</param>
         /// <param name="numberOfTasksGrantedConcurrently">The number of tasks that granted concurrently. </param>
         /// <typeparam name="T">The type</typeparam>
         /// <returns>The <see cref="Task"/>. </returns>
-        public static async Task WhenAllByThrottlingAsync<T>(IEnumerable<T> items, Func<T, Task> taskFunc, int numberOfTasksGrantedConcurrently)
+        public static async Task<ConcurrentDictionary<T, Exception>> WhenAllByThrottlingAsync<T>(IEnumerable<T> items, Func<T, Task> func, int numberOfTasksGrantedConcurrently)
         {
+            var failures = new ConcurrentDictionary<T, Exception>();
+
             using (var semaphore = new SemaphoreSlim(numberOfTasksGrantedConcurrently))
             {
                 var tasks = items.Select(async item =>
@@ -22,7 +32,11 @@
                         await semaphore.WaitAsync();
                         try
                         {
-                            await taskFunc(item);
+                            await func(item);
+                        }
+                        catch (Exception e)
+                        {
+                            failures.TryAdd(item, e);
                         }
                         finally
                         {
@@ -31,6 +45,9 @@
                     });
 
                 await Task.WhenAll(tasks);
+
+                return failures;
             }
         }
     }
+}
